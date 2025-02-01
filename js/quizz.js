0aab19794f7bee61890c2ef02cf414b41e5865bb
moonn23 <hoang230116@gmail.com>
2025-02-01 21:05:00 +0700

    Add files via upload

--- js/quizz.js
+++ js/quizz.js
@@ -25,3 +25,37 @@ function undo() {
     bp.innerHTML="Hover over an image to display here!";
 }
 
+function setImage() {
+    const currentImages = document.querySelectorAll(".preview")
+    for (let i=0; i < currentImages.length; i++) {
+        console.log("Image"+i)
+        currentImages[i].setAttribute("tabindex","0")
+    }
+}
+function focusFunction() {
+    document.getElementById("input").style.background="yellow";
+}
+
+function blurFunction() {
+    document.getElementById("input").style.background="red";
+}
+
+function enterName() {
+    const input = document.getElementById("input");
+    const name=input.value;
+    let result="7 days to die";
+    if(name==result) window.alert("You are true! This game is 7 days to die!");
+    else window.alert(name+": is the false result");
+}
+
+
+function loadPicture4() {
+    options = ["sw2.jpg", "sw2.jpg", "sw4.jpg", "bw2.jpg", "bw3.jpg", "bw4.jpg", "bo2.jpg", "bo3.jpg", "bo4.jpg"];
+    randomImg = "images/" + options[Math.floor(Math.random()*options.length)];
+    img = document.getElementById("picture4");
+    img.setAttribute("src", randomImg);
+    img.setAttribute("alt","");
+}
+
+
+
